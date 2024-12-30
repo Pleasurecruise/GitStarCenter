1caed8c85f3287d1ad6b05680679b983890b1b6a
自由的世界人 <3196812536@qq.com>
2024-12-30 15:36:28 +0800

    update: 脱敏

--- src/main/java/cn/yiming1234/gitstarcenter/config/CorsConfig.java
+++ src/main/java/cn/yiming1234/gitstarcenter/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package cn.yiming1234.gitstarcenter.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,11 +13,13 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowedOrigins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://gitstar.center"));
-//        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
